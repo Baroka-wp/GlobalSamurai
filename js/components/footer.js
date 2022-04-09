490e3133af9dbf05544767dc703a06108d628408
baroka <birotori@gmail.com>
2022-04-09 14:30:33 +0100

    Add componnent feature for footer and header

---
+++ js/components/footer.js
@@ -0,0 +1,28 @@
+export default class Footer extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    this.render();
+  }
+  render() {
+    this.innerHTML = `
+      <footer>
+        <div class="footer-content">
+          <div class="footer-credit">
+            <h2>GS</h2>
+            <h3>Global <br> Samurai</h3>
+          </div>
+          <div class="copyrigth">
+            <h4>2015 Creative Commons Korea. Some Rights Reserved.</h4>
+            <br>
+            <p>
+              このサイトでは、SAMURAI School of Engineeringの活動をご紹介しています。ウェブサイトでは、会社の活動内容やアクター、目的などを簡潔に紹介しています。
+              <a href="https://africasamurai.com/">https://africasamurai.com/</a>
+            </p>
+          </div>
+        </div>
+      </footer>
+    `;
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
+import Header from '../js/components/header.js';
+import Footer from '../js/components/footer.js';
+
+
+customElements.define("v-header", Header);
+customElements.define("v-footer", Footer);
+
 const hamburger = document.querySelector('.hamburger');
 const navbar = document.querySelector('.nav-bar');
 const main = document.querySelector('.main');
+const navLink = document.querySelectorAll('.nav-link');
+
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navbar.classList.toggle('active');
   main.classList.toggle('active');
 })
+
+navLink.forEach(link => {
+  link.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navbar.classList.remove('active');
+    main.classList.remove('active');
+  })
+});
